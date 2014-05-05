6f39bd2599ece51d4f20c45b5b2fe00c4aadf9a2
Ben Gamari <bgamari.foss@gmail.com>
2014-05-05 05:53:12 -0400

    queue: Use Unsafe instead of Cell
    Unsafe is slightly easier to statically initialize

--- src/lib/queue.rs
+++ src/lib/queue.rs
@@ -2,17 +2,17 @@
 // all links are uint to allow for static initialization
 
 use hal::cortex_m3::sched::CritSection;
-use std::cell::Cell;
+use std::ty::Unsafe;
 use std::option::{Option,Some,None};
 
 pub struct Node<T> {
-    pub next: Cell<*mut Node<T>>,
+    pub next: Unsafe<*mut Node<T>>,
     pub data: T
 }
 
 pub struct Queue<T> {
-    pub head: Cell<*mut Node<T>>,
-    pub tail: Cell<*mut Node<T>>
+    pub head: Unsafe<*mut Node<T>>,
+    pub tail: Unsafe<*mut Node<T>>
 }
 
 fn null_mut<T>() -> *mut T { 0 as *mut T }
@@ -20,44 +20,44 @@ fn null_mut<T>() -> *mut T { 0 as *mut T }
 impl<T> Queue<T> {
     pub fn new() -> Queue<T> {
         Queue {
-            head: Cell::new(null_mut()),
-            tail: Cell::new(null_mut())
+            head: Unsafe::new(null_mut()),
+            tail: Unsafe::new(null_mut())
         }
     }
 
     /// Push to head
     pub unsafe fn push(&self, node: *mut Node<T>, _: &CritSection) {
-        if self.head.get() == (0 as *mut Node<T>) {
-            self.tail.set(node);
+        if *self.head.get() == (0 as *mut Node<T>) {
+            *self.tail.get() = node;
         }
-        (*node).next.set(0 as *mut Node<T>);
-        self.head.set(node);
+        *(*node).next.get() = 0 as *mut Node<T>;
+        *self.head.get() = node;
     }
 
     /// Peek at tail
     pub unsafe fn peek(&self) -> Option<*mut Node<T>> {
-        let a = self.tail.get();
-        if a == null_mut() {
+        let tail = self.tail.get();
+        if *tail == null_mut() {
             None
         } else {
-            Some(a)
+            Some(*tail)
         }
     }
 
     /// Pop off of tail
     pub unsafe fn pop(&self, _: &CritSection) -> Option<*mut Node<T>> {
-        let a = self.tail.get();
-        if a == null_mut() {
+        let tail = self.tail.get();
+        if *tail == null_mut() {
             None
         } else {
-            self.tail.set((*self.tail.get()).next.get());
-            Some(a)
+            *tail = *(**tail).next.get();
+            Some(*tail)
         }
     }
 }
 
 impl<T> Node<T> {
     pub fn new(data: T) -> Node<T> {
-        Node { next: Cell::new(null_mut()), data: data }
+        Node { next: Unsafe::new(null_mut()), data: data }
     }
 }
