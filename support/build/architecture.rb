e3a36aa77525b34f5233b012b46bc3679323b48b
Vladimir Pouzanov <farcaller@gmail.com>
2014-05-07 19:52:26 +0100

    Fixed yaml dependency name.

--- support/build/architecture.rb
+++ support/build/architecture.rb
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require 'YAML'
+require 'yaml'
 
 class Architecture
   attr_reader :name
--- support/build/platform.rb
+++ support/build/platform.rb
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require 'YAML'
+require 'yaml'
 
 class Platform
   attr_reader :name, :arch_name, :features
