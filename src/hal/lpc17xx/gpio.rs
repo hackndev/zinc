5d3bbf40814833ab06dcea9140787412dc746e39
Vladimir Pouzanov <farcaller@gmail.com>
2014-05-08 19:52:04 +0100

    Added basic gpio interrupt handling.

--- src/hal/lpc17xx/gpio.rs
+++ src/hal/lpc17xx/gpio.rs
@@ -15,7 +15,11 @@
 
 //! GPIO configuration.
 
+use std::option::{Option, None};
+use std::intrinsics::abort;
+
 use hal::gpio::{Direction, In, Out, Level, Low, High};
+use hal::gpio::{InterruptEdge, Rising, Falling};
 use super::pin::{PinConf, Port0, Port1, Port2, Port3, Port4};
 
 #[path="../../lib/ioreg.rs"] mod ioreg;
@@ -82,6 +86,23 @@ impl<'a> GPIO<'a> {
     }
   }
 
+  pub fn set_interrupt_handler(&self, edge: InterruptEdge,
+      h: Option<GPIOHandler>) {
+    let b = match self.pin.port {
+      Port0 => match edge {
+        Rising  => GPIO0Rising,
+        Falling => GPIO0Falling,
+      },
+      Port2 => match edge {
+        Rising  => GPIO2Rising,
+        Falling => GPIO2Falling,
+      },
+      _ => unsafe { abort() },
+    };
+    set_gpio_handler(b, self.pin.pin, h)
+  }
+
+  #[inline(always)]
   fn reg(&self) -> &reg::GPIO {
     match self.pin.port {
       Port0 => &reg::GPIO0,
@@ -93,15 +114,142 @@ impl<'a> GPIO<'a> {
   }
 }
 
+pub type GPIOHandler = fn();
+type GPIOBank = [Option<GPIOHandler>, ..32];
+
+// TODO(farcaller): those static muts are accessed from both userland and isr,
+// so they must be synchronized in some way. Apparently, no scheduling should
+// happen while in gpio isr.
+static mut GPIO0RisingBank: GPIOBank = [None, ..32];
+static mut GPIO0FallingBank: GPIOBank = [None, ..32];
+static mut GPIO2RisingBank: GPIOBank = [None, ..32];
+static mut GPIO2FallingBank: GPIOBank = [None, ..32];
+
+enum GPIOInterruptHandlerBank {
+  GPIO0Rising,
+  GPIO0Falling,
+  GPIO2Rising,
+  GPIO2Falling,
+}
+
+impl GPIOInterruptHandlerBank {
+  pub fn bank(self) -> &mut GPIOBank {
+    unsafe {
+      match self {
+        GPIO0Rising  => &mut GPIO0RisingBank,
+        GPIO0Falling => &mut GPIO0FallingBank,
+        GPIO2Rising  => &mut GPIO2RisingBank,
+        GPIO2Falling => &mut GPIO2FallingBank,
+      }
+    }
+  }
+}
+
+fn set_gpio_handler(bank: GPIOInterruptHandlerBank, pin: u8,
+    h: Option<GPIOHandler>) {
+  bank.bank()[pin as uint] = h;
+}
+
+// TODO(farcaller): h shouldn't be called from isr, it actually should be passed
+// onto corresponding user task stack. DO NOT allow user code to run in MSP!
+// TODO(farcaller): this code is made with cmd+c / cmd+v.
+#[cfg(gpio_isr)]
+#[inline(always)]
+pub unsafe fn isr_gpio() {
+  let mut rise0 = reg::GPIOINT.IO0IntStatR();
+  let mut fall0 = reg::GPIOINT.IO0IntStatF();
+  let mut rise2 = reg::GPIOINT.IO2IntStatR();
+  let mut fall2 = reg::GPIOINT.IO2IntStatF();
+
+  while rise0 > 0 {
+    let bitloc = 31 - count_leading_zeroes(rise0);
+    let bank = GPIO0Rising.bank();
+    let handler = bank[bitloc];
+    match handler {
+      Some(h) => h(),
+      None => (),
+    }
+    let bit: u32 = 1 << bitloc;
+    reg::GPIOINT.set_IO0IntClr(bit);
+    rise0 -= bit;
+  }
+
+  while fall0 > 0 {
+    let bitloc = 31 - count_leading_zeroes(fall0);
+    let bank = GPIO0Falling.bank();
+    let handler = bank[bitloc];
+    match handler {
+      Some(h) => h(),
+      None => (),
+    }
+    let bit: u32 = 1 << bitloc;
+    reg::GPIOINT.set_IO0IntClr(bit);
+    fall0 -= bit;
+  }
+
+  while rise2 > 0 {
+    let bitloc = 31 - count_leading_zeroes(rise2);
+    let bank = GPIO2Rising.bank();
+    let handler = bank[bitloc];
+    match handler {
+      Some(h) => h(),
+      None => (),
+    }
+    let bit: u32 = 1 << bitloc;
+    reg::GPIOINT.set_IO2IntClr(bit);
+    rise2 -= bit;
+  }
+
+  while fall2 > 0 {
+    let bitloc = 31 - count_leading_zeroes(fall2);
+    let bank = GPIO2Falling.bank();
+    let handler = bank[bitloc];
+    match handler {
+      Some(h) => h(),
+      None => (),
+    }
+    let bit: u32 = 1 << bitloc;
+    reg::GPIOINT.set_IO2IntClr(bit);
+    fall2 -= bit;
+  }
+}
+
+#[inline(always)]
+fn count_leading_zeroes(val: u32) -> u8 {
+  let out: u32;
+  unsafe {
+    asm!("clz $0, $1" : "=r"(out) : "r"(val) :: "volatile");
+  }
+  out as u8
+}
+
 mod reg {
   use lib::volatile_cell::VolatileCell;
 
-  ioreg!(GPIO: FIODIR, _r0, _r1, _r2, FIOMASK, FIOPIN, FIOSET, FIOCLR)
+  ioreg!(GPIO: FIODIR, _pad_0, _pad_1, _pad_2, FIOMASK, FIOPIN, FIOSET, FIOCLR)
   reg_rw!(GPIO, FIODIR,  set_FIODIR,  FIODIR)
   reg_rw!(GPIO, FIOMASK, set_FIOMASK, FIOMASK)
   reg_rw!(GPIO, FIOPIN,  set_FIOPIN,  FIOPIN)
   reg_rw!(GPIO, FIOSET,  set_FIOSET,  FIOSET)
   reg_rw!(GPIO, FIOCLR,  set_FIOCLR,  FIOCLR)
+
+  ioreg!(GPIOINTReg: IOIntStatus,
+      IO0IntStatR, IO0IntStatF, IO0IntClr, IO0IntEnR, IO0IntEnF,
+      _pad_0, _pad_1, _pad_2,
+      IO2IntStatR, IO2IntStatF, IO2IntClr, IO2IntEnR, IO2IntEnF)
+  reg_r!( GPIOINTReg, IOIntStatus,                IOIntStatus)
+
+  reg_r!( GPIOINTReg, IO0IntStatR,                IO0IntStatR)
+  reg_r!( GPIOINTReg, IO0IntStatF,                IO0IntStatF)
+  reg_w!( GPIOINTReg,              set_IO0IntClr, IO0IntClr)
+  reg_rw!(GPIOINTReg, IO0IntEnR,   set_IO0IntEnR, IO0IntEnR)
+  reg_rw!(GPIOINTReg, IO0IntEnF,   set_IO0IntEnF, IO0IntEnF)
+
+  reg_r!( GPIOINTReg, IO2IntStatR,                IO2IntStatR)
+  reg_r!( GPIOINTReg, IO2IntStatF,                IO2IntStatF)
+  reg_w!( GPIOINTReg,              set_IO2IntClr, IO2IntClr)
+  reg_rw!(GPIOINTReg, IO2IntEnR,   set_IO2IntEnR, IO2IntEnR)
+  reg_rw!(GPIOINTReg, IO2IntEnF,   set_IO2IntEnF, IO2IntEnF)
 
   extern {
     #[link_name="iomem_GPIO0"] pub static GPIO0: GPIO;
@@ -109,5 +257,7 @@ mod reg {
     #[link_name="iomem_GPIO2"] pub static GPIO2: GPIO;
     #[link_name="iomem_GPIO3"] pub static GPIO3: GPIO;
     #[link_name="iomem_GPIO4"] pub static GPIO4: GPIO;
+
+    #[link_name="iomem_GPIOINT"] pub static GPIOINT: GPIOINTReg;
   }
 }
