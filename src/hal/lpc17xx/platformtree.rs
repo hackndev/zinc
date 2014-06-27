709b59efd1ccdb05f1aeba0749864ca1cb796eb5
Vladimir Pouzanov <farcaller@gmail.com>
2014-06-27 19:28:59 +0100

    Ported UART demo to PT. Broken, as PT now needs cross-node dependency tracking.

--- src/hal/lpc17xx/platformtree.rs
+++ src/hal/lpc17xx/platformtree.rs
@@ -38,6 +38,11 @@ pub fn build_mcu(builder: &mut Builder, cx: &mut ExtCtxt,
     None
   });
 
+  node.get_by_path("uart").and_then(|sub| -> Option<bool> {
+    build_uart(builder, cx, sub);
+    None
+  });
+
   node.get_by_path("gpio").and_then(|sub| -> Option<bool> {
     build_gpio(builder, cx, sub);
     None
@@ -293,4 +298,90 @@ pub fn build_gpio(builder: &mut Builder, cx: &mut ExtCtxt,
       builder.add_main_statement(st);
     }
   }
+}
+
+pub fn build_uart(builder: &mut Builder, cx: &mut ExtCtxt,
+    node: &Gc<node::Node>) {
+  if !node.expect_no_attributes(cx) { return }
+
+  for (path, sub) in node.subnodes.iter() {
+    let uart_peripheral_str = format!("UART{}",
+        match from_str::<uint>(path.as_slice()).unwrap() {
+          0|2|3 => path,
+          other => {
+            cx.parse_sess().span_diagnostic.span_err(sub.path_span,
+                format!("unknown UART `{}`, allowed values: 0, 2, 3",
+                    other).as_slice());
+            continue;
+          }
+        });
+    let uart_peripheral = TokenString::new(uart_peripheral_str);
+
+    if sub.name.is_none() {
+      cx.parse_sess().span_diagnostic.span_err(sub.name_span,
+          "UART node must have a name");
+      continue;
+    }
+
+    if !sub.expect_attributes(cx, [
+        ("baud_rate", node::IntAttribute),
+        ("mode", node::StrAttribute),
+        ("tx", node::RefAttribute),
+        ("rx", node::RefAttribute)]) {
+      continue;
+    }
+
+    let baud_rate: u32 = sub.get_int_attr("baud_rate").unwrap() as u32;
+    let mode = sub.get_string_attr("mode").unwrap();
+    let tx_node_name = sub.get_ref_attr("tx").unwrap();
+    let rx_node_name = sub.get_ref_attr("rx").unwrap();
+
+    let word_len = mode.as_slice().char_at(0).to_digit(10).unwrap() as u8;
+    let parity = TokenString::new(
+        match mode.as_slice().char_at(1) {
+          'N' => "Disabled",
+          'O' => "Odd",
+          'E' => "Even",
+          '1' => "Forced1",
+          '0' => "Forced0",
+          _ => fail!(),
+        }.to_str());
+    let stop_bits = mode.as_slice().char_at(2).to_digit(10).unwrap() as u8;
+    build_uart_gpio(builder, from_str(path.as_slice()).unwrap(),
+        tx_node_name.as_slice(), true);
+    build_uart_gpio(builder, from_str(path.as_slice()).unwrap(),
+        rx_node_name.as_slice(), false);
+
+    sub.type_name.set(Some("zinc::hal::lpc17xx::uart::UART"));
+    let uart_name = TokenString::new(sub.name.clone().unwrap());
+    let uart_name_conf = TokenString::new(format!("{}_conf", sub.name.clone().unwrap()));
+
+    let st_conf = quote_stmt!(&*cx,
+        let $uart_name_conf = {
+          use zinc::hal;
+          use zinc::hal::lpc17xx::uart;
+          uart::UARTConf {
+            peripheral: uart::$uart_peripheral,
+            baudrate: $baud_rate,
+            word_len: $word_len,
+            parity: hal::uart::$parity,
+            stop_bits: $stop_bits,
+          }
+        }
+    );
+    let st = quote_stmt!(&*cx, let $uart_name = $uart_name_conf.setup() );
+    builder.add_main_statement(st_conf);
+    builder.add_main_statement(st);
+  }
+}
+
+pub fn build_uart_gpio(builder: &Builder, uart_idx: uint, name: &str,
+    istx: bool) {
+  let node = builder.pt.get_by_name(name).unwrap();
+  let direction = (if istx {"out"} else {"in"}).to_str();
+  let function = format!("{}{}", if istx {"txd"} else {"rxd"}, uart_idx);
+  node.attributes.borrow_mut().insert("direction".to_str(),
+        node::Attribute::new_nosp(node::StrValue(direction)));
+  node.attributes.borrow_mut().insert("function".to_str(),
+        node::Attribute::new_nosp(node::StrValue(function)));
 }
--- src/hal/lpc17xx/platformtree_test.rs
+++ src/hal/lpc17xx/platformtree_test.rs
@@ -224,3 +224,54 @@ fn builds_altfn_gpio() {
         "let p3 = p3_conf.setup();");
   });
 }
+
+#[test]
+fn builds_uart() {
+  with_parsed("
+    mcu {
+      clock {
+        system_frequency = 100_000_000;
+      }
+    }
+    uart {
+      uart@0 {
+        baud_rate = 9600;
+        mode = \"8N1\";
+        tx = &uart_tx;
+        rx = &uart_rx;
+      }
+    }
+    gpio {
+      uart_tx@0;
+      uart_rx@1;
+    }
+    ", |cx, failed, pt| {
+    let mut builder = Builder::new(pt);
+    lpc17xx_pt::build_uart(&mut builder, cx, pt.get_by_path("uart").unwrap());
+    assert!(unsafe{*failed} == false);
+    assert!(builder.main_stmts.len() == 2);
+
+    assert_equal_source(builder.main_stmts.get(0),
+        "let uart_conf = {
+          use zinc::hal;
+          use zinc::hal::lpc17xx::uart;
+          uart::UARTConf {
+            peripheral: uart::UART0,
+            baudrate: 9600u32,
+            word_len: 8u8,
+            parity: hal::uart::Disabled,
+            stop_bits: 1u8,
+          }
+        };");
+    assert_equal_source(builder.main_stmts.get(1),
+        "let uart = uart_conf.setup();");
+
+    let tx_node = pt.get_by_name("uart_tx").unwrap();
+    assert!(tx_node.get_string_attr("direction").unwrap() == "out".to_str());
+    assert!(tx_node.get_string_attr("function").unwrap() == "txd0".to_str());
+
+    let rx_node = pt.get_by_name("uart_rx").unwrap();
+    assert!(rx_node.get_string_attr("direction").unwrap() == "in".to_str());
+    assert!(rx_node.get_string_attr("function").unwrap() == "rxd0".to_str());
+  });
+}
