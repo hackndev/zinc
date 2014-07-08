628cf626481164a497ef02615ba76bc39b91320f
Ben Gamari <bgamari.foss@gmail.com>
2014-07-08 16:42:36 -0400

    k20: Rework SIM with ioregs!

--- src/hal/k20/sim.rs
+++ src/hal/k20/sim.rs
@@ -18,34 +18,143 @@
 pub mod reg {
   use lib::volatile_cell::VolatileCell;
 
-  #[allow(uppercase_variables)]
-  struct SIM {
-    SOPT1:    VolatileCell<u32>,
-    SOPT1CFG: VolatileCell<u32>,
-    _pad0:    [VolatileCell<u32>, ..(0x1004 - 0x8) / 4],
-    SOPT2:    VolatileCell<u32>,
-    _pad1:    VolatileCell<u32>,
-    SOPT4:    VolatileCell<u32>,
-    SOPT5:    VolatileCell<u32>,
-    _pad2:    VolatileCell<u32>,
-    SOPT7:    VolatileCell<u32>,
-    _pad3:    [VolatileCell<u32>, ..(0x1024 - 0x101c) / 4],
-    SDID:     VolatileCell<u32>,
-    _pad4:    [VolatileCell<u32>, ..(0x1034 - 0x1028) / 4],
-    SCGC4:    VolatileCell<u32>,
-    SCGC5:    VolatileCell<u32>,
-    SCGC6:    VolatileCell<u32>,
-    SCGC7:    VolatileCell<u32>,
-    CLKDIV1:  VolatileCell<u32>,
-    CLKDIV2:  VolatileCell<u32>,
-    FCFG1:    VolatileCell<u32>,
-    FCFG2:    VolatileCell<u32>,
-  }
+  ioregs!(FTM = {
+    0x0    => reg32 sopt1 {
+      12..15 => ramsize: ro,
+      19..18 => osc32ksel,
+      29     => usbvstby,
+      30     => usbstby,
+      31     => usbregen,
+    },
 
-  reg_rw!(SIM, u32, SOPT5,  set_SOPT5,  SOPT5)
-  reg_rw!(SIM, u32, SCGC5,  set_SCGC5,  SCGC5)
+    0x4    => reg32 sopt1cfg {
+      24     => urwe,
+      25     => uvswe,
+      26     => usswe,
+    },
 
-  extern {
-    #[link_name="iomem_SIM"] pub static SIM: SIM;
-  }
+    0x1004 => reg32 sopt2 {
+      4      => rtcclkoutsel,
+      5..7   => clkoutsel,
+      11     => ptd7pad,
+      12     => traceclksel,
+      16     => pllfllsel,
+      18     => usbsrc,
+    },
+
+    0x100c => reg32 sopt4 {
+      0      => ftm0flt0,
+      1      => ftm0flt1,
+      4      => ftm1flt0,
+      18..19 => ftm1ch0src,
+      24     => ftm0clksel,
+      25     => ftm1clksel,
+      28     => ftm0trg0src,
+    },
+
+    0x1010 => reg32 sopt5 {
+      0      => uart0txsrc,
+      2..3   => uart0rxsrc,
+      4      => uart1txsrc,
+      6..7   => uart1rxsrc,
+    },
+
+    0x1018 => reg32 sopt7 {
+      0..3   => adc0trgsel {
+        0x0 => PDB0_EXTRG,
+        0x1 => CMP0_OUT,
+        0x2 => CMP1_OUT,
+        // reserved
+        0x4 => PIT_TRG0,
+        0x5 => PIT_TRG1,
+        0x6 => PIT_TRG2,
+        0x7 => PIT_TRG3,
+        0x8 => FTM0_TRG,
+        0x9 => FTM1_TRG,
+        // unused
+        // unused
+        0x12 => RTC_ALARM,
+        0x13 => RTC_SECONDS,
+        0x14 => LPT_TRG,
+        // unused
+      },
+      4      => adc0pretrgsel,
+      7      => adc0alttrgen,
+    },
+
+    0x1034 => reg32 scgc4 {
+      1      => ewm,
+      2      => cmt,
+      6      => i2c0,
+      10     => uart0,
+      11     => uart1,
+      12     => uart2,
+      18     => usbotg,
+      19     => cmp,
+      20     => vreg,
+    },
+
+    0x1038 => reg32 scgc5 {
+      0      => lptimer,
+      5      => tsi,
+      9      => porta,
+      10     => portb,
+      11     => portc,
+      12     => portd,
+      13     => porte,
+    },
+
+    0x103c => reg32 scgc6 {
+      0      => ftfl,
+      1      => dmamux,
+      12     => spi0,
+      15     => i2s,
+      18     => crc,
+      21     => usbdcd,
+      22     => pdb,
+      23     => pit,
+      24     => ftm0,
+      25     => ftm1,
+      27     => adc0,
+      29     => rtc,
+    },
+
+    0x1040 => reg32 scgc7 {
+      1      => dma,
+    },
+
+    0x1044 => reg32 clkdiv1 {
+      16..19 => outdiv4,
+      24..27 => outdiv2,
+      28..31 => outdiv1,
+    },
+
+    0x1048 => reg32 clkdiv2 {
+      0      => usbfrac,
+      1..3   => usbdiv,
+    },
+
+    0x104c => reg32 fcfg1 {
+      0      => flashdis,
+      1      => flashdoze,
+      8..11  => depart,
+      16..19 => eesize,
+      24..27 => pfsize,
+      28..31 => nvmsize,
+    },
+
+    0x1050 => reg32 fcfg2 {
+      16..22 => maxaddr1,
+      23     => pflsh,
+      24..30 => maxaddr0,
+    },
+
+    0x1058 => reg32 uidmh {
+      0..31  => uid,
+    },
+
+    0x1060 => reg32 uidl {
+      0..31  => uid,
+    },
+  })
 }
