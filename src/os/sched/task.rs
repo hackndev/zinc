5dadda6ae0d803721f8df40b634dcefcb8818014
Vladimir Pouzanov <farcaller@gmail.com>
2014-07-23 19:09:41 +0100

    Added docs to os::sched::task

--- src/os/sched/task.rs
+++ src/os/sched/task.rs
@@ -13,21 +13,36 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/// Task structures.
+
 use core::kinds::marker;
 
+/// Task states.
 pub enum State {
+  /// This task can be scheduled.
   Runnable
 }
 
+/// Task descriptor. Should be treated as opaque struct.
 pub struct Task {
+  /// Current task state.
   pub state: State,
+
+  /// Pointer to top of the stack.
   pub stack_start: u32,
+
+  /// Pointer to the lowest possible stack address.
   pub stack_end: u32,
 }
 
+/// Tasks index provides a scheduler with a list of all registered tasks and
+/// initial state.
 #[packed]
 pub struct TasksIndex<'a> {
+  /// A mutabler slice with all defined tasks.
   pub tasks: &'a mut [Task],
+
+  /// Current running task index.
   pub current_task_index: u8,
 
   pub no_copy: marker::NoCopy,
