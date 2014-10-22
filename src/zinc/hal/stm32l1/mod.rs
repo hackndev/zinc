687f2767eaf2f81e95b75f66211f2cfc741f2c5e
Dzmitry Malyshau <kvarkus@gmail.com>
2014-10-22 07:55:53 -0400

    stm32l1 - removed the clock code that is not yet ported, fixed the copyright.

--- src/zinc/hal/stm32l1/mod.rs
+++ src/zinc/hal/stm32l1/mod.rs
@@ -1,5 +1,5 @@
 // Zinc, the bare metal stack for rust.
-// Copyright 2014 Vladimir "farcaller" Pouzanov <farcaller@gmail.com>
+// Copyright 2014 Dzmitry "kvark" Malyshau <kvarkus@gmail.com>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
