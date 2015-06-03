4e7d6f82d4e7c5c7c8995eeb81d53206eb54ac76
Vladimir Pouzanov <farcaller@gmail.com>
2015-06-03 21:33:07 +0100

    Added jenkins build script

---
+++ support/build-jenkins.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e
+
+if [ "$PLATFORM" == "native" ]; then
+  # build unit tests
+  cargo test --lib --verbose
+  (cd ./ioreg; cargo build --verbose; cargo test --verbose)
+  (cd ./platformtree; cargo build --verbose; cargo test --verbose)
+  (cd ./macro_platformtree; cargo build --verbose; cargo test --verbose)
+  (cd ./macro_zinc; cargo test --verbose)
+else
+  # build cross-compiled lib and examples
+  case "$PLATFORM" in
+    lpc17xx )
+      TARGET=thumbv7m-none-eabi
+      EXAMPLES="blink blink_pt uart dht22 empty"
+      ;;
+    k20 )
+      TARGET=thumbv7em-none-eabi
+      EXAMPLES="blink_k20 blink_k20_isr empty"
+      ;;
+    stm32f4 )
+      TARGET=thumbv7em-none-eabi
+      EXAMPLES="blink_stm32f4 empty"
+      ;;
+    stm32l1 )
+      TARGET=thumbv7m-none-eabi
+      EXAMPLES="blink_stm32l1 bluenrg_stm32l1 usart_stm32l1 empty"
+      ;;
+  esac
+
+  ./configure --host=arm-none-eabi
+  cargo build --target=$TARGET --verbose --features "mcu_$PLATFORM"
+
+  for e in $EXAMPLES; do
+    EXAMPLE_NAME=$e make build
+  done
+fi
