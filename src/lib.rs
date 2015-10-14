a9531ed4f949c3f65c689a0d253518ed4f071afc
Nick Twyman <nick@conky.org>
2015-10-13 21:30:21 -0700

    Updated src/lib.rs to no long call cout core_str_ext as a feature. It's been stabalized in nightly

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@
 // limitations under the License.
 
 #![feature(asm, lang_items, plugin, range_inclusive)]
-#![feature(core_intrinsics, core_slice_ext, core_str_ext)]
+#![feature(core_intrinsics, core_slice_ext)]
 #![allow(improper_ctypes)]
 #![feature(const_fn)]
 #![deny(missing_docs)]
