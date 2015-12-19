5abcd8000ff7bec4c6273252e4d59ae699d631f3
Krzysztof Blazewicz <blazewicz.krzysztof@gmail.com>
2015-12-20 00:17:00 +0100

    stm32f1-example: for SYSCLK use PLL fed by HSI

--- examples/blink_stm32f1/src/main.rs
+++ examples/blink_stm32f1/src/main.rs
@@ -13,8 +13,22 @@ fn main() {
   zinc::hal::mem_init::init_stack();
   zinc::hal::mem_init::init_data();
 
-  let sys_clock = init::ClockConfig::new_default();
-  //sys_clock.setup();
+   // configure PLL and set it as System Clock source
+  let pll_conf = init::PllConf {
+    source: init::PllClockSource::PllSourceHSIDiv2,
+    mult: init::PllMult::PllMul12,
+    hse_prediv: init::PllHsePrediv::PllHsePrediv1,
+    usb_prescaler: init::PllUsbDiv::PllUsbDiv1,
+  };
+  let sys_clock = init::ClockConfig {
+      source:         init::SystemClockSource::SystemClockPLL(pll_conf),
+      ahb_prescaler:  init::ClockAhbPrescaler::AhbDivNone,
+      apb1_prescaler: init::ClockApbPrescaler::ApbDiv2,
+      apb2_prescaler: init::ClockApbPrescaler::ApbDivNone,
+      flash_latency:  init::FlashLatency::FlashLatency1,
+      mco:            init::McoSource::McoClockPLL,
+  };
+  sys_clock.setup();
 
   let led1 = pin::Pin::new(pin::Port::PortC, 13, pin::PinConf::OutPushPull50MHz);
 
